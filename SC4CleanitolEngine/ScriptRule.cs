bfceb858c7d7568dc3f94e18c4dd1af7a9c6e36c
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2026-03-26 21:09:53 -0400

    update script rule parsing

--- SC4CleanitolEngine/ScriptRule.cs
+++ SC4CleanitolEngine/ScriptRule.cs
@@ -1,128 +1,27 @@
 ﻿using System.Text.RegularExpressions;
+using csDBPF;
 
-namespace SC4Cleanitol {
+namespace SC4CleanitolEngine {
     /// <summary>
     /// Classes, enums, and functions related to parsing script rules. A rule is a single line read from the script.
     /// </summary>
-    internal class ScriptRule {
-        private static readonly string[] dbpfExtensions = { ".dat", ".sc4lot", ".sc4desc", ".sc4model", ".dll" };
-
+    internal static class ScriptRule {
         /// <summary>
-        /// A script rule targeted at locating required files or TGIs.
+        /// Defines an action that should be taken when the rule is processed.
         /// </summary>
-        public struct DependencyRule {
-            /// <summary>
-            /// Whether this rule is a conditional dependency rule or a standard dependency rule.
-            /// </summary>
-            public bool IsConditionalRule { get; set; }
-
-            /// <summary>
-            /// Filename or TGI to search for.
-            /// </summary>
-            public string SearchItem { get; set; }
-            /// <summary>
-            /// Scan for the SearchItem if this item is present. Can be a filename or TGI.
-            /// </summary>
-            public string ConditionalItem { get; set; }
-            /// <summary>
-            /// Is the SearchItem a TGI?
-            /// </summary>
-            public bool IsSearchItemTGI { get; set; }
-            /// <summary>
-            /// Is the ConditionalItem a TGI?
-            /// </summary>
-            public bool IsConditionalItemTGI { get; set; }
-            /// <summary>
-            /// Name of the exchange upload containing the SearchItem.
-            /// </summary>
-            public string SourceName { get; set; }
-            /// <summary>
-            /// URL of the exchange upload containing the SearchItem.
-            /// </summary>
-            public string SourceURL { get; set; }
-
-            /// <summary>
-            /// Indicates a dependency rule where no valid TGI or file was provided to scan for.
-            /// </summary>
-            /// <remarks>
-            /// Provided for compatibility reasons for very old cleanitol scripts. 
-            /// </remarks>
-            internal bool IsUnchecked { get; private set; }
-
-            /// <summary>
-            /// A script rule targeted at locating required files or TGIs.
-            /// </summary>
-            /// <param name="ruleText">Raw text for this rule</param>
-            public DependencyRule(string ruleText) {
-                int semicolonLocn = ruleText.IndexOf(';');
-                int conditionalLocn = ruleText.IndexOf("??");
-                IsConditionalRule = conditionalLocn != -1;
-
-                //Support "unchecked" dependencies for certain legacy cleanitol files that use these as cascading dependencies. If searchItem does not contain any of the valid file extensions AND is not a TGI (contains '0x' 3 times).
-                int cutoff;
-                if (conditionalLocn == -1) {
-                    cutoff = semicolonLocn;
-                } else {
-                    cutoff = conditionalLocn;
-                }
-                if (!dbpfExtensions.Any(x => ruleText.Substring(0, semicolonLocn).ToLower().EndsWith(x)) && Regex.Matches(ruleText.Substring(0, cutoff), "0x").Count != 3) {
-                    IsUnchecked = true;
-                }
-
-                
-                if (IsConditionalRule) {
-                    SearchItem = ruleText.Substring(0, conditionalLocn).Trim();
-                    ConditionalItem = ruleText.Substring(conditionalLocn + 2, semicolonLocn - conditionalLocn - 2).Trim();
-                    IsConditionalItemTGI = ConditionalItem.Substring(0, 2) == "0x";
-                } else {
-                    SearchItem = ruleText.Substring(0, semicolonLocn).Trim();
-                    ConditionalItem = string.Empty;
-                    IsConditionalItemTGI = false;
-                }
-
-                IsSearchItemTGI = SearchItem.Substring(0, 2) == "0x";
-                int httpLocn = ruleText.IndexOf("http");
-                if (httpLocn - semicolonLocn + 1 > 4) {
-                    SourceName = ruleText.Substring(semicolonLocn + 1, httpLocn - semicolonLocn - 2).Trim();
-                } else {
-                    SourceName = SearchItem;
-                }
-                if (httpLocn == -1) {
-                    SourceURL = "Unspecified URL";
-                } else {
-                    SourceURL = ruleText.Substring(httpLocn).Trim();
-                }
-                    
-
-                // The script can allow for any separator between TGI numbers, but csDBPF uses comma space ", ". Automatically reformat the script format behind the scenes to allow for equality comparison.
-                if (IsSearchItemTGI) {
-                    SearchItem = csDBPF.DBPFTGI.CleanTGIFormat(SearchItem);
-                }
-                if (IsConditionalItemTGI) {
-                    ConditionalItem = csDBPF.DBPFTGI.CleanTGIFormat(ConditionalItem);
-                }
-            }
-        }
-
-
-
-        /// <summary>
-        /// The action to be taken for this rule.
-        /// </summary>
-        /// <see ref="https://www.sc4devotion.com/forums/index.php?topic=3797.0"/>
-        public enum RuleType {
+        public enum Type {
             /// <summary>
             /// Internal comments not shown to the user. Use for script documentation.
             /// </summary>
-            ScriptComment,
+            HiddenComment,
             /// <summary>
             /// Text shown to the user in the script output window.
             /// </summary>
-            UserComment,
+            Comment,
             /// <summary>
             /// A heading shown to the user in the script output window. Handy to separate out content.
             /// </summary>
-            UserCommentHeading,
+            Heading,
             /// <summary>
             /// A web url to import rules from. Must point to a <c>.txt</c> file.
             /// </summary>
@@ -145,45 +44,122 @@ namespace SC4Cleanitol {
             Invalid
         }
 
-
-
         /// <summary>
-        /// Determine the action to be taken for this ruleText.
+        /// Parse the syntax of the specified rule to determine its type.
         /// </summary>
         /// <param name="rule">Rule text</param>
-        /// <returns>A <see cref="RuleType"/> informing the action to be taken</returns>
-        public static RuleType ParseRuleType(string rule) {
-            //Script Comment
-            if (rule.IndexOf(';') == 0 || rule.Length == 0) {
-                return RuleType.ScriptComment;
+        /// <returns>A <see cref="Type"/> informing the action to be taken.</returns>
+        internal static Type Parse(string rule) {
+            //Previous headings started with >#. This was a mistake. Should have used regular markdown-style syntax instead. Though the `>` is fine for comments because there'd otherwise be no way to differentiate them from rules.
+            if (rule.StartsWith(">#")) {
+                rule = rule.Replace(">#", "#");
             }
 
-            //User Comment + User Comment Heading
-            if (rule.IndexOf('>') == 0) {
-                if (rule.IndexOf('#') == 1) {
-                    return RuleType.UserCommentHeading;
-                }
-                return RuleType.UserComment;
-            }
-
-            //Import + Dependency + Conditional Dependency
-            if (rule.Contains("http", StringComparison.OrdinalIgnoreCase)) {
-                if (rule.StartsWith("@")) {
-                    return RuleType.Import;
+            //Historic cleanitol schema: https://www.sc4devotion.com/forums/index.php?topic=3797.0. The New format is backwards compatible with this schema.
+            if (rule.StartsWith(';') || rule.Length == 0) {
+                return Type.HiddenComment;
+            } else if (rule.StartsWith('#')) {
+                return Type.Heading;
+            } else if (rule.StartsWith('>')) {
+                return Type.Comment;
+            } else if (rule.Contains("http", StringComparison.OrdinalIgnoreCase)) {
+                if (rule.StartsWith('@')) {
+                    return Type.Import;
                 } else if (rule.Contains("??")) {
-                    return RuleType.ConditionalDependency;
+                    return Type.ConditionalDependency;
                 } else if (rule.Contains(';')) {
-                    return RuleType.Dependency;
+                    return Type.Dependency;
                 } else {
-                    return RuleType.Invalid;
+                    return Type.Invalid;
                 }
-            }
-
-            //Support "unchecked" dependencies for certain legacy cleanitol files that use these as cascading dependencies.
-            if (rule.IndexOf(';') > 0) {
-                return RuleType.Dependency;
+            } else if (rule.IndexOf(';') > 0) { //Support "unchecked" dependencies for certain legacy cleanitol files that use these as cascading dependencies.
+                return Type.Dependency;
             } else {
-                return RuleType.Removal;
+                return Type.Removal;
+            }
+        }
+
+        /// <summary>
+        /// A script rule targeted at locating required files or TGIs.
+        /// </summary>
+        internal class DependencyRule {
+            /// <summary>
+            /// Whether this rule is a conditional dependency rule or a standard dependency rule.
+            /// </summary>
+            public bool IsConditional { get; private set; }
+            /// <summary>
+            /// Filename or TGI to search for.
+            /// </summary>
+            public string SearchItem { get; private set; }
+            /// <summary>
+            /// Scan for the SearchItem if this item is present. Can be a filename or TGI.
+            /// </summary>
+            public string ConditionalItem { get; private set; }
+            /// <summary>
+            /// Name of the exchange upload the <see cref="SearchItem"/> is found at.
+            /// </summary>
+            public string LinkName { get; private set; }
+            /// <summary>
+            /// URL of the exchange upload containing the <see cref="SearchItem"/>.
+            /// </summary>
+            public string LinkUrl { get; private set; }
+
+            /// <summary>
+            /// Indicates a dependency rule where no valid TGI or file was provided to scan for.
+            /// </summary>
+            /// <remarks>
+            /// Provided for compatibility reasons for very old cleanitol scripts. 
+            /// </remarks>
+            internal bool IsUnchecked { get; private set; }
+
+            /// <summary>
+            /// A script rule targeted at locating required files or TGIs.
+            /// </summary>
+            /// <param name="ruleText">Raw text for this rule</param>
+            internal DependencyRule(string ruleText) {
+                int semicolonLocn = ruleText.IndexOf(';');
+                int conditionalLocn = ruleText.IndexOf("??");
+                IsConditional = conditionalLocn != -1;
+
+                //Support "unchecked" dependencies for certain legacy cleanitol files that use these as cascading dependencies. If searchItem does not contain any of the valid file extensions AND is not a TGI (contains '0x' 3 times).
+                int cutoff;
+                if (conditionalLocn == -1) {
+                    cutoff = semicolonLocn;
+                } else {
+                    cutoff = conditionalLocn;
+                }
+                if (!DBPFUtil.SC4Extensions.Any(ext => ruleText.Contains(ext, StringComparison.CurrentCultureIgnoreCase) && Regex.Matches(ruleText.Substring(0, cutoff), "0x").Count != 3)) {
+                    IsUnchecked = true;
+                }
+
+                if (IsConditional) {
+                    SearchItem = ruleText.Substring(0, conditionalLocn).Trim();
+                    ConditionalItem = ruleText.Substring(conditionalLocn + 2, semicolonLocn - conditionalLocn - 2).Trim();
+                } else {
+                    SearchItem = ruleText.Substring(0, semicolonLocn).Trim();
+                    ConditionalItem = string.Empty;
+                }
+
+                int httpLocn = ruleText.IndexOf("http");
+                if (httpLocn - semicolonLocn + 1 > 4) {
+                    LinkName = ruleText.Substring(semicolonLocn + 1, httpLocn - semicolonLocn - 2).Trim();
+                } else {
+                    LinkName = SearchItem;
+                }
+                if (httpLocn == -1) {
+                    LinkUrl = "Unspecified URL";
+                } else {
+                    LinkUrl = ruleText.Substring(httpLocn).Trim();
+                }
+
+
+                // The script can allow for any separator between TGI numbers, but csDBPF uses comma space ", ". Automatically reformat the script format behind the scenes to allow for equality comparison.
+                if (SearchItem.StartsWith("0x")) {
+                    SearchItem = DBPFUtil.FormatTgiString(SearchItem);
+                }
+                if (ConditionalItem.StartsWith("0x")) {
+                    ConditionalItem = DBPFUtil.FormatTgiString(ConditionalItem);
+                }
             }
         }
     }
